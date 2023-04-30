52ad450eabcbb0ff0d409c1559209e563448c100
Andrei Mihai Adrian <mihai.andrei1597@gmail.com>
2023-04-30 20:00:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,5 @@ app = FastAPI()
 async def root():
     return {"message": "Hello from FastAPI!"}
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
